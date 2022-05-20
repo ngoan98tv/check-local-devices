1ffb809ec1ae4ce23fa4552e5d25597535e517af
Ngoan Tran <ngoan98travinh@gmail.com>
2022-05-20 13:02:35 +0700

    setup slack notify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import { MongoClient } from "mongodb";
 import find from "local-devices";
 import { appConfig } from "./config";
+import { execSync } from "child_process";
 
 const client = new MongoClient(appConfig.MONGO_URI);
 
-async function notifyChange(message) {}
+async function notifyChange(message) {
+  const payload = {
+    text: message,
+    icon_emoji: ":bot:",
+    username: "ARP Bot",
+  };
+  let retried = 0;
+  let error = true;
+  while (error && retried < 5) {
+    try {
+      const result = execSync(
+        `curl -X POST --data-urlencode \
+      'payload=${JSON.stringify(payload)}' ${appConfig.SLACK_HOOK}`
+      );
+      console.log(result.toString());
+      error = false;
+    } catch (error) {
+      console.log("failed to send notification, retry...");
+      error = true;
+    }
+    retried++;
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+  }
+}
 
 async function main() {
   console.log(appConfig);
@@ -13,7 +37,7 @@ async function main() {
   const db = client.db(appConfig.MONGO_DB);
 
   const people = db.collection("people");
-  
+
   await new Promise(() => {
     console.log(`Start interval`);
     setInterval(() => {
@@ -34,10 +58,11 @@ async function main() {
           })
           .toArray()
           .then((result) => {
+            if (!result || !result[0]) return;
             const nameString = result
               ?.map((person) => person.displayName)
               ?.join(", ");
-            notifyChange(`Just online: ${nameString}`);
+            notifyChange(`${nameString} just online`);
           });
 
         // Update online status
@@ -61,10 +86,11 @@ async function main() {
           })
           .toArray()
           .then((result) => {
+            if (!result || !result[0]) return;
             const nameString = result
               ?.map((person) => person.displayName)
               ?.join(", ");
-            notifyChange(`Just offline: ${nameString}`);
+            notifyChange(`${nameString} just offline`);
           });
 
         // Update online status
@@ -89,7 +115,7 @@ async function main() {
               const previousValue = person[key];
               if (currentValue !== previousValue) {
                 notifyChange(
-                  `${person.displayName} changed ${key} from ${previousValue} to ${currentValue}`
+                  `${person.displayName} change ${key} from ${previousValue} to ${currentValue}`
                 );
               }
             }
